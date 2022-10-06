fc9908ec16b062c1a198675027be07f38355b1fd
Fsher07 <furkansalimm@gmail.com>
2022-10-06 19:14:23 +0300

    Create skills and project sections

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com', 'cdn.jsdelivr.net'],
+    domains: ['res.cloudinary.com', 'cdn.jsdelivr.net', 'images.g2crowd.com'],
   },
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import About from '../components/About';
 import Experience from '../components/Experience';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import styles from '../styles/Home.module.css';
 
@@ -36,7 +37,9 @@ const Home: NextPage = () => {
         <Skills />
       </section>
       {/* Projects Section */}
-
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
       {/* Contact Me Section */}
     </div>
   );
