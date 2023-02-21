68f75f217d9204078d7572a952a8a63351f2862c
Furkan Salim Hıdır <furkansalimm@gmail.com>
2023-02-21 20:39:17 +0300

    add new vercel endpoint for experiences

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import axios from 'axios';
-import ExperienceCard from './ExperienceCard';
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import axios from "axios";
+
+import { baseUrl } from "../services/services";
+import ExperienceCard from "./ExperienceCard";
 
 type Props = {};
 
@@ -18,22 +20,21 @@ interface Experience {
 const Experience = ({}: Props) => {
   const [experience, setExperience] = useState<any>(null);
   useEffect(() => {
-    axios
-      .get('https://portfoliomongodb.herokuapp.com/api/experiences')
-      .then((res) => {
-        setExperience(res.data);
-      });
+    axios.get(`${baseUrl}/experiences`).then((res) => {
+      setExperience(res.data);
+    });
   }, []);
+  console.log(`${baseUrl}/experiences`);
   return (
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className='sectionTitle'>Experience</h3>
+      <h3 className="sectionTitle">Experience</h3>
 
-      <div className='flex w-full space-x-8 overflow-x-scroll p-10 snap-x snap-mandatory overflow-y-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+      <div className="flex w-full space-x-8 overflow-x-scroll p-10 snap-x snap-mandatory overflow-y-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {experience
           ? experience.map((exp: Experience) => (
               <ExperienceCard
